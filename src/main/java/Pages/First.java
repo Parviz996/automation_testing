3d269b63ec620138d5394ea80b1c5f79549c886b
Parviz996 <parvizbaxseliyev996@gmail.com>
2025-01-23 09:43:37 +0400

    test

--- src/main/java/Pages/First.java
+++ src/main/java/Pages/First.java
@@ -1,4 +1,14 @@
 package Pages;
 
+import org.openqa.selenium.By;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.chrome.ChromeDriver;
+
 public class First {
+    public static void main(String[] args) {
+        WebDriver driver = new ChromeDriver();
+        driver.get(("https://www.oxu.az/"));
+        driver.findElement(By.xpath("//button[@class='custom-navbar-toggle']")).click();
+    }
+
 }
