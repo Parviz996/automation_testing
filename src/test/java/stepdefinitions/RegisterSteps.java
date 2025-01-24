34e88a1d09fd3ac5c71766c0d7e2befcb4d1768c
Parviz996 <parvizbaxseliyev996@gmail.com>
2025-01-24 17:53:28 +0400

    T24.2

--- src/test/java/stepdefinitions/RegisterSteps.java
+++ src/test/java/stepdefinitions/RegisterSteps.java
@@ -1,11 +1,14 @@
 package stepdefinitions;
 
 import io.cucumber.java.en.And;
+import org.openqa.selenium.By;
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.chrome.ChromeDriver;
 import pages.RegisterPage;
 import utilities.ConfigReader;
 import utilities.Driver;
+
+import static utilities.Driver.driver;
 
 public class RegisterSteps {
 
@@ -41,37 +44,56 @@ public class RegisterSteps {
 
     @And("User writes City")
     public void userWritesCity() {
+        register.city.sendKeys((ConfigReader.getProperty("city")));
     }
 
     @And("User writes State")
     public void userWritesState() {
+        register.state.sendKeys((ConfigReader.getProperty("state")));
     }
 
     @And("User writes Zipcode")
     public void userWritesZipcode() {
+        register.zipCode.sendKeys((ConfigReader.getProperty("zip")));
     }
 
     @And("User writes Phone")
     public void userWritesPhone() {
+        register.phoneNumber.sendKeys(ConfigReader.getProperty("phone"));
     }
 
     @And("User writes SSN")
     public void userWritesSSN() {
+        register.ssn.sendKeys((ConfigReader.getProperty("ssn")));
     }
 
     @And("User writes Username")
     public void userWritesUsername() {
+        register.username.sendKeys((ConfigReader.getProperty("username")));
     }
 
     @And("User writes Password")
     public void userWritesPassword() {
+        register.password.sendKeys((ConfigReader.getProperty("password")));
     }
 
     @And("User writes Confirm")
     public void userWritesConfirm() {
+        register.repeatedPassword.sendKeys((ConfigReader.getProperty("password")));
     }
 
     @And("User clicks Register button")
     public void userClicksRegisterButton() {
+        register.registerButton.click();
+
+        String expectedResult = "Your account was created successfully. You are now logged in.";
+        String actualResult =  driver.findElement(By.xpath("//p[contains(text(),'Your account was created successfully. You are now')]")).getText();
+
+        if (expectedResult.equals(actualResult)) {
+            System.out.println("Test uğurla tamamlandı.");
+        } else {
+            System.out.println("Test uğursuz oldu: Expected = " + expectedResult + ", Actual = " + actualResult);
+        }
     }
+
 }
